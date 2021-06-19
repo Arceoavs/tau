ac174b8f5621c56de2809c36f846874bd72ae21d
Arnold Arz <arnoarz99@gmail.com>
2021-06-19 15:03:34 +0300

    ♻️ extracting programinto file

--- index.pl
+++ index.pl
@@ -1,12 +1,9 @@
-# lol
 item(id(1), name(bread)).
 item(id(2), name(water)).
 item(id(3), name(apple)).
 
-
 shop(id(1), name(tau), location(spain)).
 shop(id(2), name(swi), location(netherlands)).
-
 
 stock(item(1), shop(1), count(23), price(0.33)).
 stock(item(2), shop(1), count(17), price(0.25)).
